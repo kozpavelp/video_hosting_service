871a1629acaddba75f7fc80535fe75fa49396fe2
Pavel <kozpavelp@gmail.com>
2023-09-04 14:48:28 -0500

    Docker bugfix

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ env = Env()
 DB_URL = env.str(
     "DB_URL", default="postgresql+asyncpg://postgres:postgres@0.0.0.0:5432/postgres"
 )
-
+# ROMOTE IP FOR MIGRATIONS
+# DB_URL = env.str("DB_URL", default="postgresql+asyncpg://postgres:postgres@146.70.149.217:5432/postgres")
 TEST_DB_URL = env.str(
     "TEST_DB_URL",
     default="postgresql+asyncpg://postgres_test:postgres_test@0.0.0.0:5433/postgres_test",
